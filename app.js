0db25fc04ce775621f00fc955e9720fe40a7980c
Fishdestroyer <jeffwhitner@gmail.com>
2022-02-03 22:51:05 -0600

    commit

--- app.js
+++ app.js
@@ -8,15 +8,18 @@ console.log(profileDataArgs);*/
 /*const [name, github] = profileDataArgs;
 console.log(name, github);*/
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?'
-    }
-  ])
-  .then(answers => console.log(answers));
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      }
+    ]);
+  };
+  
+  promptUser().then(answers => console.log(answers));
+  
   const pageHTML = generatePage(name, github);
 fs.writeFile('./index.html', pageHTML, err => {
     if (err) throw err;
