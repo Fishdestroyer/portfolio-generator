9c2724e4b7e31311ecc2b31f785667dd55fa4be4
Fishdestroyer <jeffwhitner@gmail.com>
2022-02-03 22:49:19 -0600

    added and tested inquirer

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./src/page-templates');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+/*const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);*/
 
-const [name, github] = profileDataArgs;
-console.log(name, github);
+/*const [name, github] = profileDataArgs;
+console.log(name, github);*/
 
-const pageHTML = generatePage(name, github);
-
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+  const pageHTML = generatePage(name, github);
 fs.writeFile('./index.html', pageHTML, err => {
     if (err) throw err;
         console.log('Portfolio complete! check out index.html to see the output')
